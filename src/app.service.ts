c645a586b48ba94efc6b9440e814706aa4da0649
jin.park <jin.park@asurion.com>
2021-06-18 23:10:50 +0900

    Add post

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const hello: string = 'Hello World!';
+    const hello: string = 'Hello!';
     console.log(hello);
+    return hello;
+  }
+
+  sayHello(): string {
+    const hello: string = 'Say Hello!';
+    console.log('sayHello():', hello);
     return hello;
   }
 }
