b2b65d716d936cbcdd63bf3010ee65b331c1a614
preciouStar <seongjin605@users.noreply.github.com>
2021-06-22 15:00:57 +0900

    Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user/UserController';
 import { UserService } from './user/UserService';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'test',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
