f30c28b0b6d06871f123ea01e4019108a4ba3bab
preciouStar <qkrtjdwls605@naver.com>
2021-06-28 23:55:56 +0900

    update

--- src/user/UserDto.ts
+++ src/user/UserDto.ts
@@ -1,3 +1,14 @@
 export class UserDto {
-  public static UserReadDto = class {};
+  private id: number | null = null;
+
+  public getId(): number | null {
+    return this.id;
+  }
+
+  public UserReadDto = new (class {
+    constructor(public superThis: UserDto) {}
+    public testSetOuterPrivate(target: number) {
+      this.superThis.id = target;
+    }
+  })(this);
 }
--- src/user/UserService.ts
+++ src/user/UserService.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './UserDto';
 
 @Injectable()
 export class UserService {
