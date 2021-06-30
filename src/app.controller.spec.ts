b30a77f7d351a9d31ef090c0bc9f4187b806cefa
preciouStar <qkrtjdwls605@naver.com>
2021-06-30 23:52:01 +0900

    Update test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './user/UserController';
+import { UserService } from './user/UserService';
+import { ReadUserDto } from './user/ReadUserDto';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let userController: UserController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    userController = app.get<UserController>(UserController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(
+        userController.getUser(new ReadUserDto('jin.park', 23, 'male')),
+      ).toBe({});
     });
   });
 });
